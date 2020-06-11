ce0a9ee9e70cd0d468b3bf4df75c6e596722a81f
Gerrit van Gelder <Gerrit@van-gelder.de>
2020-06-12 00:18:08 +0200

    Testfiles
    Added some examples for testing and also as reference for new features.

---
+++ example/run_examples.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Generating basic android and ios files, without tags and flavors"
+../twine generate-localization-file ./source.json ./output/android_basic.xml -f android -l de
+../twine generate-localization-file ./source.json ./output/ios_basic.strings -f apple -l de
+
+echo "Generating the ios tagged subset of strings"
+../twine generate-localization-file ./source.json ./output/ios_only.strings -f apple -l de -t ios
