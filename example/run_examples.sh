3199ff11a9a885586b7b885d1d5012dbae1bf5bf
Gerrit van Gelder <Gerrit@van-gelder.de>
2020-11-11 16:30:56 +0100

    - altered example runner - also changed source file, we wanted \n not \\n - incorporated changes into android and apple formatter

--- example/run_examples.sh
+++ example/run_examples.sh
@@ -2,12 +2,10 @@
 
 echo "Generating basic android and ios files, without tags and flavors"
 ../twine generate-localization-file ./source.json ./output/android_basic.xml -f android -l de
-../twine generate-localization-file ./source.json ./output/ios_basic.strings -f apple -l de
+../twine generate-localization-file ./source.json ./output/ios_basic.strings -f apple -l de --kill-all-tags
 
 echo "Generating the ios tagged subset of strings"
 ../twine generate-localization-file ./source.json ./output/ios_only.strings -f apple -l de -t ios
 
-echo "Kill Tag Option Test, see HTML Tags"
-../twine generate-localization-file ./source.json ./output/android_basic_wo_tags.xml -f android -l de --kill-all-tags
-
 echo "Generating a flavored version of the strings"
+echo "TODO..."
